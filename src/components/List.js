4d08d00af7f7a2f2646de3bc9f02a764ac2e4b39
Svasti <22300286@students.ltu.edu.au>
2024-09-01 16:57:12 +1000

    React state successfully implemented

--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,46 @@
 function Task(props) {
+	
+	console.log(props)
+
+	function onChange() {
+		// Find the task we want to update and update it
+		props.setTasks(tasks => tasks.map(task => {
+			if (task.id === props.id) {
+				return {
+					id: task.id,
+					description: task.description,
+					completed: !task.completed
+				};
+			} else {
+				return task;
+			} 
+		}));
+	}
+
 	return (
-		<li>{ props.description } <input type="checkbox" checked={props.completed} readOnly /></li>
+		<li>
+			{ props.description } 
+			<input 
+				type="checkbox" 
+				checked={props.completed} 
+				onChange={onChange} 
+			/>
+		</li>
 	);
 }
 
 function List(props) {
-
 	return (
 		<div>
 			<h1>{ props.heading }</h1>
 			<ul>
-				{ props.tasks.map(task => <Task description={task.description} completed={task.completed} />) }
+				{ props.tasks.map(task => 
+					<Task 
+						setTasks={props.setTasks}
+						id={task.id}
+						description={task.description} 
+						completed={task.completed} 
+					/>) }
 			</ul>
 		</div>
 	);
